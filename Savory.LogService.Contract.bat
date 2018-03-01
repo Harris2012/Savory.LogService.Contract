7af0740649005869109922d72b2b32e2a744e061
HarrisZhang <hc_zhang@Ctrip.com>
2018-03-01 11:29:21 +0800

    update nupkg version

--- Savory.LogService.Contract.bat
+++ Savory.LogService.Contract.bat
@@ -2,8 +2,14 @@ msbuild Savory.LogService.Contract.Net40\Savory.LogService.Contract.Net40.csproj
 
 msbuild Savory.LogService.Contract.Net45\Savory.LogService.Contract.Net45.csproj /t:rebuild /p:configuration=release;DocumentationFile=bin\Release\Savory.LogService.Contract.xml;DebugType=none
 
+pushd Savory.LogService.Contract.Net45
+reference^
+ /csproj:Savory.LogService.Contract.Net45.csproj^
+ /target:savory-lib\Savory.LogService.Contract.txt
+popd
+
 nuget pack Savory.LogService.Contract.nuspec
 
-move /y Savory.LogService.Contract.*.nupkg D:\LocalWebRoot\SavoryNuget\Packages
+@rem move /y Savory.LogService.Contract.*.nupkg D:\LocalWebRoot\SavoryNuget\Packages
 
 pause
